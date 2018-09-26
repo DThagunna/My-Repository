3f28a701e7ff4e713e6c88912460112902211c63
adminuser <adminuser@localhost.localdomain>
2018-09-26 15:28:15 -0400

    trile commit

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo  "Git is a very well supported open source project with over a decade of solid stewardship. The project maintainers have shown balanced judgment and a mature approach to meeting the long term needs of its users with regular releases that improve usability and functionality. The quality of the open source software is easily scrutinized and countless businesses rely heavily on that quality."
+
